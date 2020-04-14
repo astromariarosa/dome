257ed0236957a929bdeae868f5752b7f2ef7d57f
Mariarosa Marinelli <mariarosamarinelli@gmail.com>
2020-04-14 13:49:57 -0400

    Add files via upload
    Script to write a VOTable from updated FRB data

---
+++ UpdateFRB.py
@@ -0,0 +1,74 @@
+# To run this script, first download the newest FRB Catalog from frbcat.org
+# Just click Export to CSV - do not alter columns. Save to the same folder
+# where this script is located. Run this script with the command line:
+# python UpdateFRB.py <frbcat_date.csv>
+
+# Mariarosa Marinelli
+# mariarosamarinelli@gmail.com
+
+import sys
+import numpy as np
+import astropy
+from astropy.io import ascii
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy.io.votable import parse
+
+filenames = []
+
+if (len(sys.argv) == 2):
+    for fname in sys.argv[1:]:
+        try:
+            file = open(fname, 'r')
+            file.close()
+            filenames.append(fname)
+
+            filename = sys.argv[1]
+
+            print("%s is valid, thank you." % fname)
+        except:
+            print("%s not found." % fname)
+
+else:
+    print("Please include the FRB Catalog datafile.")
+
+def first():
+    data = ascii.read(filename, format='csv', fast_reader=False)
+
+    na = np.array(data[0][:])
+    di = np.array(data[2][:])
+    r = np.array(data[3][:])
+    d = np.array(data[4][:])
+
+    coord = SkyCoord(r, d, unit='deg')
+    ra = coord.ra.degree
+    de = coord.dec.degree
+
+    tbl = Table(data=(na, di, ra, de),
+                names=('FRBname', 'discoloc', 'ra', 'dec'),
+                dtype=('str', 'str', 'float64', 'float64'))
+
+    astropy.io.votable.writeto(tbl, '%s.xml' % filename[0:-4], tabledata_format=None)
+
+def second():
+    votable = parse('FRBUpdate.xml')
+
+    mytable = votable.get_first_table()
+
+    rafield = mytable.get_field_by_id('ra')
+    rafield.ucd = 'pos.eq.ra'
+    rafield.unit = 'deg'
+
+    decfield = mytable.get_field_by_id('dec')
+    decfield.ucd = 'pos.eq.dec'
+    decfield.unit = 'deg'
+
+    votable.to_xml('%s.xml' % filename[0:-4])
+    print("VOTable has been created and updated. Good job!")
+
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        first()
+        second()
+    else:
+        print("Please try again.")

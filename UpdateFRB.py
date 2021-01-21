85e9070c92ad57300819533c4b1d8ddd6413f0b7
Mariarosa Marinelli <mariarosamarinelli@gmail.com>
2021-01-21 13:03:39 -0500

    Edited comments and print statements for UpdateFRB.py

--- UpdateFRB.py
+++ UpdateFRB.py
@@ -1,10 +1,13 @@
+# This script converts the online catalog of Fast Radio Bursts into a VOTable
+# that can then be easily processed using the Digistar Utility VOTableToDSA.
+
 # To run this script, first download the newest FRB Catalog from frbcat.org
-# Just click Export to CSV - do not alter columns. Save to the same folder
+# Just click Export to CSV - do not alter columns. Save to the same directory
 # where this script is located. Run this script with the command line:
 # python UpdateFRB.py <frbcat_date.csv>
 
 # Mariarosa Marinelli
-# mariarosamarinelli@gmail.com
+# mmarinelli@smv.org
 
 import sys
 import numpy as np
@@ -49,6 +52,7 @@ def first():
                 dtype=('str', 'str', 'float64', 'float64'))
 
     astropy.io.votable.writeto(tbl, '%s.xml' % filename[0:-4], tabledata_format=None)
+    print("VOTable has been created.")
 
 def second():
     votable = parse('FRBUpdate.xml')
@@ -64,11 +68,11 @@ def second():
     decfield.unit = 'deg'
 
     votable.to_xml('%s.xml' % filename[0:-4])
-    print("VOTable has been created and updated. Good job!")
+    print("VOTable has been updated. Good job!")
 
 if __name__ == '__main__':
     if (len(sys.argv) > 1):
         first()
         second()
     else:
-        print("Please try again.")
+        print("Something went wrong.")
